271a02795ffba0d6e0170b44ee76dd85f9da25d6
katallzxc <katallzxc@gmail.com>
2025-01-12 15:21:42 -0500

    add rotary potentiometer code files

--- modules/S1_dial/rotary_pot_LED.py
+++ modules/S1_dial/rotary_pot_LED.py
@@ -1,18 +1,21 @@
 # this code is from Raspberry Pi:
 # https://projects.raspberrypi.org/en/projects/getting-started-with-the-pico/8
 
-from machine import ADC, Pin
+from machine import ADC, PWM, Pin
 import time
 
 # set PWM/ADC constants and defaults
 DUTY_MIN = 0
 DUTY_MAX = 65535
 
+# set up LED as PWM output
+LED_PIN = 25        # this pin controls the onboard LED!
+pwm = PWM(Pin(LED_PIN))
+pwm.freq(1000)
+
 # set up rotary pot as ADC input
 SENSOR_PIN =          # enter pin number here! A0 is GPIO pin 26
 rot_pot = ADC(Pin(SENSOR_PIN))
 
 while True:
-    percent_of_max = (rot_pot.read_u16()/DUTY_MAX)*100
-    print(percent_of_max)
-    time.sleep(0.1)
+    pwm.duty_u16(rot_pot.read_u16())
