c1e3fae66bae824da5b00ee61ffb798e14404c16
katallzxc <katallzxc@gmail.com>
2025-01-12 15:56:25 -0500

    add photoresistor and thermistor code and schematics

--- modules/S3_temperature/thermistor.py
+++ modules/S3_temperature/thermistor.py
@@ -1,35 +1,15 @@
-from machine import Pin, ADC
-from time import sleep
+from machine import ADC, Pin
+import time
 
-# set display to show either:
-INT_MODE = 0        # ADC output representative integer
-VOLT_MODE = 1       # the voltage that it represents
-mode = INT_MODE
+# set ADC constants and defaults
+ADC_MIN = 0
+ADC_MAX = 65535
 
-# always 0xff (in hex) according to: https://learn.adafruit.com/
-# circuitpython-basics-analog-inputs-and-outputs/
-# analog-to-digital-converter-inputs
-ADC_HIGH = 65535 #65025?
-
-# set up thermistor as analog input
+# set up thermistor as ADC input
 SENSOR_PIN =          # enter pin number here! A0 is GPIO pin 26
-thermistor = ADC(Pin(SENSOR_PIN))
+therm = ADC(Pin(SENSOR_PIN))
 
-# show reference voltage (logic high, 3.3V) and the
-# corresponding analog integer value
-ADC_REF = thermistor.reference_voltage
-print("ADC reference voltage: {}".format(ADC_REF))
-print("ADC high voltage integer value: {}".format(ADC_HIGH))
-
-# convert ADC input value back to voltage
-def adc_to_voltage(adc_value):
-    return  ADC_REF * (float(adc_value)/float(ADC_HIGH))
-
-# take readings from thermistor
 while True:
-    val = thermistor.read_u16()
-    if mode == INT_MODE: # read adc value and print
-        print((val,))
-    else: # convert to voltage
-        print((adc_to_voltage(val),))
-    sleep(0.5)
+    percent_of_max = 100*(1 - therm.read_u16()/ADC_MAX)
+    print("thermal level: {0}".format(percent_of_max))
+    time.sleep(0.1)
