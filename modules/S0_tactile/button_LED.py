51f38733ded1a97ed871ab47d1be3368475e587f
katallzxc <katallzxc@gmail.com>
2025-01-12 15:07:44 -0500

    add button code and schematic

--- modules/S0_tactile/button_LED.py
+++ modules/S0_tactile/button_LED.py
@@ -4,12 +4,12 @@
 from machine import Pin
 from time import sleep
 
+# set up LED pin as output and button pin as input
+LED_PIN = 25        # this pin controls the onboard LED!
 BUTTON_PIN =          # enter pin number here!
+led = Pin(LED_PIN, Pin.OUT)
 button = Pin(BUTTON_PIN, Pin.IN, Pin.PULL_DOWN)
 
 while True:
-    if button.value():
-        print("pressed")
-    else:
-        print("not pressed")
+    led.value(button.value())
     sleep(0.05)
