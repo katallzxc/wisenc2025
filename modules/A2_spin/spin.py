f803b504ee85bc544dd97415b3f73aa0866fa1b6
katallzxc <katallzxc@gmail.com>
2025-01-23 16:13:01 -0500

    add updated DC code

--- modules/A2_spin/spin.py
+++ modules/A2_spin/spin.py
@@ -6,15 +6,23 @@ DUTY_MIN = 0
 DUTY_MAX = 65535
 DEF_DUTY = int(DUTY_MAX/2)
 
-# set up DC motor as PWM output
-MOTOR_PIN =           # enter pin number here!
-pwm = PWM(Pin(MOTOR_PIN))
+# set up one DC motor pin as PWM output (to pulse commands)
+MOTOR_PIN_1 =           # enter pin number here!
+pwm = PWM(Pin(MOTOR_PIN_1))
 pwm.freq(1000)
 
+# set up other DC motor pin as digital output to GND
+MOTOR_PIN_2 =           # enter pin number here!
+dir1 = Pin(MOTOR_PIN_2, Pin.OUT)
+dir1.off()
+dir2 = Pin(16, Pin.OUT)
+dir2.on()
+
+
 while True:
-    for duty in range(DUTY_MIN, DEF_DUTY, 1):
+    for duty in range(DUTY_MIN, DUTY_MAX, 1):
         pwm.duty_u16(duty)
         sleep(0.0001)
-    for duty in range(DEF_DUTY, DUTY_MIN, -1):
+    for duty in range(DUTY_MAX, DUTY_MIN, -1):
         pwm.duty_u16(duty)
         sleep(0.0001)
