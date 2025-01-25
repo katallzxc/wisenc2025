794aac5aa81bc6f5cfaebda95d7db6e5f9d4d6f9
katallzxc <katallzxc@gmail.com>
2025-01-25 10:49:54 -0500

    tweak vibrate and spin code

--- modules/A1_vibrate/vibrate.py
+++ modules/A1_vibrate/vibrate.py
@@ -4,6 +4,7 @@ from time import sleep
 # set PWM constants and defaults
 DUTY_MIN = 0
 DUTY_MAX = 65535
+DEF_DUTY = int(2*DUTY_MAX/3)
 
 # set up vibration motor as PWM output
 MOTOR_PIN =           # enter pin number here!
@@ -11,9 +12,10 @@ pwm = PWM(Pin(MOTOR_PIN))
 pwm.freq(1000)
 
 while True:
-    for duty in range(DUTY_MIN, DUTY_MAX, 1):
+    print("starting new round of vibration")
+    for duty in range(DUTY_MIN, DEF_DUTY, 1):
         pwm.duty_u16(duty)
         sleep(0.0001)
-    for duty in range(DUTY_MAX, DUTY_MIN, -1):
+    for duty in range(DEF_DUTY, DUTY_MIN, -1):
         pwm.duty_u16(duty)
         sleep(0.0001)
--- modules/A2_spin/spin.py
+++ modules/A2_spin/spin.py
@@ -7,22 +7,14 @@ DUTY_MAX = 65535
 DEF_DUTY = int(DUTY_MAX/2)
 
 # set up one DC motor pin as PWM output (to pulse commands)
-MOTOR_PIN_1 =           # enter pin number here!
-pwm = PWM(Pin(MOTOR_PIN_1))
+MOTOR_PIN =           # enter pin number here!
+pwm = PWM(Pin(MOTOR_PIN))
 pwm.freq(1000)
 
-# set up other DC motor pin as digital output to GND
-MOTOR_PIN_2 =           # enter pin number here!
-dir1 = Pin(MOTOR_PIN_2, Pin.OUT)
-dir1.off()
-dir2 = Pin(16, Pin.OUT)
-dir2.on()
-
-
 while True:
-    for duty in range(DUTY_MIN, DUTY_MAX, 1):
+    for duty in range(DEF_DUTY, DUTY_MAX, 1):
         pwm.duty_u16(duty)
         sleep(0.0001)
-    for duty in range(DUTY_MAX, DUTY_MIN, -1):
+    for duty in range(DUTY_MAX, DEF_DUTY, -1):
         pwm.duty_u16(duty)
         sleep(0.0001)
